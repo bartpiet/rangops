dca62e10a816160265bb251059b98b72edf79d3d
Bartosz Pietraszko <bartosz.pietraszko@gmail.com>
2018-09-12 12:34:01 +0200

    initial implementation of union method

--- lib/rangops/operators.rb
+++ lib/rangops/operators.rb
@@ -1,8 +1,15 @@
 module Rangops
   module Operators
 
-    def union
-      "foo"
+    def union(other)
+      unless other.is_a? Range
+        raise ArgumentError, "expected a Range, got #{other.class} instead"
+      end
+      unless (cover?(other.first) || other.cover?(first))
+        raise ArgumentError, "non overlapping ranges"
+      end
+      b, e = [self.begin, other.begin].min, [self.end, other.end].max
+      b..e
     end
 
   end
