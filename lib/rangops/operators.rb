a3e661a93e374f736e65f9252a747038a98f976e
Bartosz Pietraszko <bartosz.pietraszko@gmail.com>
2018-09-16 23:20:28 +0200

    working intersection method

--- lib/rangops/operators.rb
+++ lib/rangops/operators.rb
@@ -14,5 +14,18 @@ module Rangops
     end
     alias_method :|, :union
 
+    # Set intersection of 2 ranges.  Returns nil if ranges don't overlap.
+    def intersection(other)
+      unless other.is_a?(Range)
+        raise ArgumentError, "expected a Range, got #{other.class} instead"
+      end
+      return nil unless overlaps?(other)
+
+      lower, upper = Utils.sort_by_end(self, other)
+      new_begin = [self.begin, other.begin].max
+      Range.new(new_begin, lower.end, lower.exclude_end?)
+    end
+    alias_method :&, :intersection
+
   end
 end
