3f433738bd129871321d1bfd99d19bd98be6eddb
Bartosz Pietraszko <bartosz.pietraszko@gmail.com>
2018-09-17 20:37:03 +0200

    basic test cases for operations

--- test/rangops_test.rb
+++ test/rangops_test.rb
@@ -1,11 +1,39 @@
 require "test_helper"
 
+require "bundler/setup"
+require "rangops"
+
+class Range
+  include Rangops
+end
+
+
 class RangopsTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Rangops::VERSION
+
+
+  def setup
+
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_union
+    assert_equal (1..5).union(3..10), (1..10)
+    assert_equal (3..10).union(1..5), (1..10)
+    assert_equal (1..5).union(3...10), (1...10)
+
+    assert_equal ('a'..'j').union('g'..'m'), ('a'..'m')
+    assert_equal ('g'..'z').union('c'..'k'), ('c'..'z')
   end
+
+  def test_intersection
+    assert_equal (1..5).intersection(3..10), (3..5)
+  end
+
+  def test_complement
+    assert_equal (1..5).complement(3..10), (5..10)
+  end
+
+  def test_difference
+    assert_equal (1..5).difference(3..10), [(1..3), (5..10)]
+  end
+
 end
